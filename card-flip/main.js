977108f744260f70ab4064f872bb64b920dd4df3
dipteshh <dmohanta_be20@thapar.edu>
2021-05-07 17:06:39 +0530

    flip&cal modiied 2

--- card-flip/main.js
+++ card-flip/main.js
@@ -2,9 +2,3 @@ const cards= document.querySelectorAll(".card-inner")
 function flipbtn(obj , i){
   cards[i-1].classList.toggle('is-flipped')
 }
-for(i=1;i<=cards.length;i++)
-{
-    console.log(cards[i-1])
-    
-}
-console.log(cards)
